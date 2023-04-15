bdaed2ac03c997dcb28e57cdbc9ed5d95c0179fb
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 01:07:51 +0300

    Add files via upload

---
+++ esp32-web/logs.php
@@ -0,0 +1,13 @@
+<?php include "parts/header.php"; ?>
+<?php include "parts/navbar.php"; ?>
+<div>
+    
+    <div class="container my-3">
+        <div class="row">
+            
+        </div>
+    </div>
+
+    <?php include "parts/footer.php"; ?>
+</div>
+</html>
