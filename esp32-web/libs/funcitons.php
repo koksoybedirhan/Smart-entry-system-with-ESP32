a787cc50b590105452c72d82e157ee5fbfb78ecc
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 01:08:23 +0300

    Add files via upload

---
+++ esp32-web/libs/funcitons.php
@@ -0,0 +1,11 @@
+<?php 
+    function getData()
+    {
+        include "db.php";
+    
+        $query = "SELECT * from veri ORDER BY ID DESC LIMIT 1";
+        $result = mysqli_query($baglanti, $query);
+        mysqli_close($baglanti);
+        return $result;
+    }
+?>
