e763d074c8c8313f3e78299ac8e8009a2c3e2b89
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 01:08:40 +0300

    Add files via upload

---
+++ esp32-web/parts/navbar.php
@@ -0,0 +1,34 @@
+<style>
+    button:hover
+    {
+        color: black;
+        background: white;
+        border-radius: 5px;
+        border: 1px solid black;
+    }
+</style>
+
+<body>
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
+                <li><a href="./index.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Ana Sayfa</button></a></li>
+                <li><a href="./logs.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Veri Kayıtları</button></a></li>
+
+            </ul>
+
+            <div class="text-end">
+                
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
+                <li><a href="./login.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Giriş</button></a></li>
+                <li><a href="./register.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Üye Olma</button></a></li>
+                   
+            </div>
+        </div>
+    </div>
+</header>
+</body>
