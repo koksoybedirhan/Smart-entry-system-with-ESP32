4caf88f0d02c048de08118e909c9bf13012a6713
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 02:11:09 +0300

    Update index.php

--- esp32-web/index.php
+++ esp32-web/index.php
@@ -1,5 +1,6 @@
 <?php
     include "libs/db.php";
+    include "libs/funcitons.php";
 
     $temperature = $humidity = "";
 
@@ -7,23 +8,57 @@
     {
         $temperature = $_GET['temp'];
         $humidity = $_GET['hum']; 
+        $query = "INSERT INTO veri (temp, hum) VALUES ('$temperature', '$humidity')";
+        $result = mysqli_query($baglanti, $query);
     }
-             
-    $query = "INSERT INTO veri (temp, hum) VALUES ('$temperature', '$humidity')";
-    $result = mysqli_query($baglanti, $query);
-    echo $temperature." / ".$humidity;
+
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === "on")
+    {
+        $url = "https://";
+    }
+    else{
+        $url = "http://";
+        $url.=$_SERVER['HTTP_HOST'];
+        $url.=$_SERVER['REQUEST_URI'];
+        $url;
+    }
+    $page = $url;
+    $sec = "9";
 ?>
 
+<?php include "parts/header.php"; ?>
+<?php include "parts/navbar.php"; ?>
+<div>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <div class="container my-3">
+        <div class="row">
+        <?php $result = getData();  while($data = mysqli_fetch_assoc($result)): ?>
+            <div class="col-6">
+                <div class="card-deck">
+                    <div class="card">
+                        <img class="card-img-top" src="img/temperature.jpeg" alt="Temperature" width="50" height="450">
+                        <div class="card-body">
+                        <h5 class="card-title" style="text-align: center;">Temperature</h5>
+                        <h6 class="card-text" style="text-align: center; font-size:50px;"><?php echo $data["temp"]; ?></h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card-deck">
+                    <div class="card">
+                        <img class="card-img-top" src="img/humidity.jpeg" alt="Temperature" width="50" height="450">
+                        <div class="card-body">
+                        <h5 class="card-title" style="text-align: center;">Humidity</h5>
+                        <h6 class="card-text" style="text-align: center; font-size:50px;"><?php echo $data["hum"]; ?></h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+
+    <?php include "parts/footer.php"; ?>
+</div>
 </html>
