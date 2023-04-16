9cb9660dbe71e0d779b571c0a57bbffe3cc883cf
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 03:18:38 +0300

    Update logs.php

--- esp32-web/logs.php
+++ esp32-web/logs.php
@@ -1,10 +1,23 @@
+<?php
+    include "libs/funcitons.php";
+?>
+
 <?php include "parts/header.php"; ?>
 <?php include "parts/navbar.php"; ?>
 <div>
     
     <div class="container my-3">
         <div class="row">
-            
+            <div class="col-8">
+                <?php $result = getAllDatas();  while($data = mysqli_fetch_assoc($result)): ?>
+                    <h6 style="text-align: center; font-size:20px;"><?php echo "Temperature: ".$data["temp"]."- Humidity:".$data["hum"]." /Time: ".$data["time"]; ?></h6>
+                <?php endwhile;?>
+            </div>
+            <div class="col-4">
+                <form action="csv.php" method="POST">
+                    <input type="submit" name="blog-create" id="submit" class="btn btn-primary">
+                </form>
+            </div>
         </div>
     </div>
 
