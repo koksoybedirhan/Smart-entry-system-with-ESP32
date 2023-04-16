855608b44ac82c174420a1ebceb3cd2c177fa48f
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 17:58:21 +0300

    Update logs.php

--- esp32-web/logs.php
+++ esp32-web/logs.php
@@ -1,4 +1,6 @@
 <?php
+    include "libs/db.php";
+    include "libs/session.php";
     include "libs/funcitons.php";
 ?>
 
@@ -9,11 +11,15 @@
     <div class="container my-3">
         <div class="row">
             <div class="col-8">
-                <?php $result = getAllDatas();  while($data = mysqli_fetch_assoc($result)): ?>
-                    <h6 style="text-align: center; font-size:20px;"><?php echo "Temperature: ".$data["temp"]."- Humidity:".$data["hum"]." /Time: ".$data["time"]; ?></h6>
-                <?php endwhile;?>
+                <div class="card">
+                    <h3 style="text-align: center;">All Datas</h3>
+                    <?php $result = getAllDatas();  while($data = mysqli_fetch_assoc($result)): ?>
+                        <h6 style="text-align: center; font-size:20px;"><?php echo "Temperature: ".$data["temp"]."- Humidity:".$data["hum"]." /Time: ".$data["time"]; ?></h6>
+                    <?php endwhile;?>
+                </div>
             </div>
-            <div class="col-4">
+            <div class="col-4" style="text-align: center;">
+                <h5>Click to show datas with excel file.</h5>
                 <form action="csv.php" method="POST">
                     <input type="submit" name="blog-create" id="submit" class="btn btn-primary">
                 </form>
