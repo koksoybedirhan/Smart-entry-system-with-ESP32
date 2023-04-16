fccb3528b0aa76f6e4374d68db7f5c48ba3d8025
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 03:19:19 +0300

    Add files via upload

---
+++ esp32-web/csv.php
@@ -0,0 +1,11 @@
+<?php 
+    include "libs/funcitons.php";
+    header("Content-type: text/plain");
+    header("Content-Disposition: attachment; filename=log.csv");
+
+    $result = getAllDatas();  while($data = mysqli_fetch_assoc($result)):
+        echo $data["temp"]." ";
+        echo $data["hum"]." ";
+        echo $data["time"]."\n";
+    endwhile;
+?>
