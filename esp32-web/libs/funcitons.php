f984c31bcb814baa5d933280005d1a705027563d
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 17:55:48 +0300

    Update funcitons.php

--- esp32-web/libs/funcitons.php
+++ esp32-web/libs/funcitons.php
@@ -18,4 +18,32 @@
         mysqli_close($baglanti);
         return $result;
     }
+
+    function getUser()
+    {
+        include "db.php";
+    
+        $query = "SELECT * from user ORDER BY ID DESC LIMIT 1";
+        $result = mysqli_query($baglanti, $query);
+        mysqli_close($baglanti);
+        return $result;
+    }
+
+    function getAllUsers()
+    {
+        include "db.php";
+    
+        $query = "SELECT * from user";
+        $result = mysqli_query($baglanti, $query);
+        mysqli_close($baglanti);
+        return $result;
+    }
+
+    function isLoggedin() {
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 ?>
