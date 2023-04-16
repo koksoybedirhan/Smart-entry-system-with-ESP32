e6ee47829b9fbd32a6061458c8539a4ab93440c2
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 17:57:22 +0300

    Update navbar.php

--- esp32-web/parts/navbar.php
+++ esp32-web/parts/navbar.php
@@ -16,17 +16,26 @@
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
 
                 <li><a href="./index.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Ana Sayfa</button></a></li>
-                <li><a href="./logs.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Veri Kayıtları</button></a></li>
-
+                <li><a href="./logUsers.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Üye Veri Kayıtları</button></a></li>
+                <li><a href="./logs.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Ortam Veri Kayıtları</button></a></li>
+                
             </ul>
 
             <div class="text-end">
                 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
 
-                <li><a href="./login.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Giriş</button></a></li>
-                <li><a href="./register.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Üye Olma</button></a></li>
-                   
+                <?php if(isLoggedin()):?>
+
+                    <li><a href="./profile.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Profile</button></a></li>
+                    <li><a href="./logout.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Logout</button></a></li>
+
+                <?php else:?>
+
+                    <li><a href="./login.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Login</button></a></li>
+                    <li><a href="./register.php" class="nav-link px-2 text-white"><button class="btn btn-outline-light me-2">Register</button></a></li>
+
+                <?php endif;?>
             </div>
         </div>
     </div>
