9e11d23aad509280f681c17d9a54bd5b0a1b7c41
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-04-16 17:58:03 +0300

    Update index.php

--- esp32-web/index.php
+++ esp32-web/index.php
@@ -1,5 +1,6 @@
 <?php
     include "libs/db.php";
+    include "libs/session.php";
     include "libs/funcitons.php";
 
     $temperature = $humidity = "";
@@ -34,28 +35,53 @@
         <div class="row">
         <?php $result = getData();  while($data = mysqli_fetch_assoc($result)): ?>
             <div class="col-6">
-                <div class="card-deck">
-                    <div class="card">
-                        <img class="card-img-top" src="img/temperature.jpeg" alt="Temperature" width="50" height="450">
-                        <div class="card-body">
-                        <h5 class="card-title" style="text-align: center;">Temperature</h5>
-                        <h6 class="card-text" style="text-align: center; font-size:50px;"><?php echo $data["temp"]; ?></h6>
-                        </div>
+                <div class="card">
+                    <div class="card-body" style="text-align: center;">
+                        <h5 class="card-title">Temperature <i class="fa-solid fa-temperature-three-quarters fa-fade"></i></h5>
+                        <p class="card-text"><?php echo $data["temp"]; ?></p>
                     </div>
                 </div>
             </div>
             <div class="col-6">
-                <div class="card-deck">
-                    <div class="card">
-                        <img class="card-img-top" src="img/humidity.jpeg" alt="Temperature" width="50" height="450">
-                        <div class="card-body">
-                        <h5 class="card-title" style="text-align: center;">Humidity</h5>
-                        <h6 class="card-text" style="text-align: center; font-size:50px;"><?php echo $data["hum"]; ?></h6>
-                        </div>
+                <div class="card">
+                    <div class="card-body" style="text-align: center;">
+                        <h5 class="card-title">Humidity <i class="fa-solid fa-droplet fa-fade"></i></h5>
+                        <p class="card-text"><?php echo $data["hum"]; ?></p>
                     </div>
                 </div>
             </div>
             <?php endwhile; ?>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3"></div>
+                        <div class="col-6">
+                            <?php if(isLoggedin()):?>
+                            <br>
+                            <?php $result2 = getUser();  while($data2 = mysqli_fetch_assoc($result2)): ?>
+                                <img class="card-img-top" src="img/<?=$_SESSION['img']?>" alt="User" height="500">
+                                <div class="card-body" style="text-align: center;">
+                                    <h5 class="card-title"><?php echo htmlspecialchars($_SESSION["name"])." (".htmlspecialchars($_SESSION["status"]).")";?></h5>
+                                    <p class="card-text">Kart Numarası: <?php echo htmlspecialchars($_SESSION["rfid"])?></p>
+                                    <a href="profile.php" class="btn btn-primary">Profile</a>
+                                </div>
+                            <?php endwhile; ?>
+                            <?php else: ?>
+                                <br>
+                                <img class="card-img-top" src="img/user.png" alt="User" height="500">
+                                <div class="card-body" style="text-align: center;">
+                                    <h5 class="card-title">Son giriş yapan:
+                                        <br>Bedirhan Köksoy (Çalışan)</h5>
+                                    <p class="card-text">Kart Numarası: 624.367.136.732</p>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
