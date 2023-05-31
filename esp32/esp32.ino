4c9f459b21917f6fe6a9f20a9db553cc921bc3be
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-05-31 20:03:10 +0300

    Update esp32.ino

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,14 +1,29 @@
-//libraries
-#include <WiFi.h>
-#include <Wire.h>
+//library part
+#include <Wire.h>             
+#include "SSD1306Wire.h"      
 #include <SFE_BMP180.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <WiFi.h>
 
-//library definitions
+//define part
+#define SS_PIN  26  /*Slave Select Pin*/
+#define RST_PIN 25  /*Reset Pin for RC522*/
+#define LED_G   12   /*Pin 8 for LED*/
+#define DEMO_DURATION 3000
+
+//library variables part
+MFRC522 mfrc522(SS_PIN, RST_PIN);   /*Create MFRC522 initialized*/
 SFE_BMP180 bmp180;
+SSD1306Wire display(0x3c, SDA, SCL);  
 
-//variables
-char bmpDurum;
-double T, P; 
+//variables part
+String deneme;
+int demoMode = 0;
+int counter = 1;
+double T, P;  // Sıcaklık ve basınç değişkenleri
+typedef void (*Demo)(void);
+String rfCode;
 
 //web connection
 const char* ssid     = "Fatih-Bedirhan";
@@ -16,16 +31,28 @@ const char* password = "Fatbed2020";
 const char* host = "192.168.1.106";
 
 void setup() {
-  //starting to serial communication
+  //starting serial communication
   Serial.begin(115200);
+
+  //starting rf id
+  SPI.begin();          
+  mfrc522.PCD_Init();   
   bmp180.begin();
   WiFi.begin(ssid, password);
+
+  //starting screen
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
 }
 
 void loop() {
+  //looping all the function
   bmp();
+  ekranYazma();
+  rf();
+  delay(200);
   
-  //connect to web server
   WiFiClient client;
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) 
@@ -34,10 +61,10 @@ void loop() {
     return;
   }
 
-  //sending data to web server with GET method
   client.print(String("GET http://localhost/esp32/index.php?") + 
                           ("temp=") + T +
                           ("&hum=") + P +
+                          ("rfCode=") + rfCode +
                           " HTTP/1.1\r\n" +
                  "Host: " + host + "\r\n" +
                  "Connection: close\r\n\r\n");
@@ -59,18 +86,69 @@ void loop() {
   delay(10000);
 }
 
-//reading and printing humidity and temperature datas.
+//rf id part
+void rf()
+{
+  //checking the card; if card is new, then going to first loop, else going second loop.
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+
+  //printing bytes of card to string and after printing string to screen.
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println(content.substring(1));
+  rfCode = content.substring(1);
+  delay(100);
+}
+
+//bmp180 part
 void bmp()
 {
-  bmpDurum = bmp180.startTemperature();
-  bmpDurum = bmp180.getTemperature(T);
-  bmpDurum = bmp180.startPressure(3);
-  bmpDurum = bmp180.getPressure(P, T);
+  //starting temperature and after getting temperature data.
+  bmp180.startTemperature();
+  delay(10);
+  bmp180.getTemperature(T);
+  delay(10);
   
+  //starting pressure and after getting pressure data with temperature data.
+  bmp180.startPressure(3);
+  delay(10);
+  bmp180.getPressure(P, T);
+
+  //printing computer and screen 
   Serial.print("Basınç: ");
   Serial.print(P);
-  Serial.print(" hPa, ");
+  Serial.println(" hPa");
   Serial.print("Sıcaklık: ");
   Serial.print(T);
   Serial.println(" C");
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 23, "Sıcaklık: " + String(T));
+  display.drawString(64, 33, "Basınç: " + String(P));
+  delay(500);
+}
+
+//selecting texts to print screen 
+Demo demos[] = {bmp};
+int demoLength = (sizeof(demos) / sizeof(Demo));
+long timeSinceLastModeSwitch = 0;
+
+//printing screen
+void ekranYazma()
+{
+  display.clear();
+  demos[demoMode]();
+  display.display();
+  delay(500);
 }
