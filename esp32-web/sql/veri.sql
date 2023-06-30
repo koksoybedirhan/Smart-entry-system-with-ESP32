74c64ab8d01b1d5165f7bea2d6bf80e73034ee7a
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-06-30 14:13:44 +0300

    Update veri.sql

--- esp32-web/sql/veri.sql
+++ esp32-web/sql/veri.sql
@@ -2,6 +2,7 @@ CREATE TABLE `veri` (
   `ID` int(11) NOT NULL,
   `temp` varchar(30) DEFAULT NULL,
   `hum` varchar(30) DEFAULT NULL,
+  `rfCode` varchar(50) NOT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -9,6 +10,7 @@ ALTER TABLE `veri`
   ADD PRIMARY KEY (`ID`);
 
 ALTER TABLE `veri`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=211;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1693;
 COMMIT;
+
 
